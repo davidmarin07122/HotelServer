ef47e514cecaf25fef79a0a57e51e4032d576ff6
tefa09 <89583766+tefa09@users.noreply.github.com>
2021-11-12 06:00:21 -0500

    se completa

--- controllers/controladorReserva.js
+++ controllers/controladorReserva.js
@@ -1,48 +1,117 @@
+const e = require('express');
 const {request, response } = require('express');
+
+const{ insertarReserva } = require ('../services/serviceHabitacion.js')
+const{ leerReserva } = require ('../services/serviceHabitacion.js')
+const{ leerHaReservas } = require ('../services/serviceHabitacion.js')
+const{ borrarReserva } = require ('../services/serviceHabitacion.js')
+const{ modificarReserva } = require ('../services/serviceHabitacion.js');
+const { leerReservaes } = require('../services/serviceReserva.js');
 
 function registrarReserva(peticion = request, respuesta = response) {
     
     //datos a recibir
-    let datosPeticion = peticion.body;
+    try {
+        
+        let datosPeticion = peticion.body;
 
-    respuesta.json({
-        mensaje:"reserva registrada..",
-        datos:datosPeticion
-    })
+        await insertarReserva(datosPeticion)
+        respuesta.status(200).json({
+            estado: true,
+            mensaje: "Exito registrando la reserva"
+        })
+    } catch (error) {
+        respuesta.status(400).json({
+            estado: false, 
+            mensaje: "Error..." + error
+        })
+    }
     
 }
 
-function consultarReservas(peticion=request, respuesta=response) {
-    respuesta.json({
-        mensaje:"buscado todo los juagdores..."
-    })
+async function consultarReservas(peticion=request, respuesta=response) {
+    
+    try {
+        
+        let havitaciones = await leerReservaes()
+
+        respuesta.status(200).json({
+            estado: true,
+            mensaje: havitaciones
+        })
+
+    } catch (error) {
+        respuesta.status(400).json({
+            estado: false,
+            mensaje: "Error" + error
+        })
+    }
 }
 
-function consultarReserva(peticion=request, respuesta=response) {
+async function consultarReserva(peticion=request, respuesta=response) {
 
-    //resibir el id a buscar
-    let id = peticion.params.id;
-
-    respuesta.json({
-        mensaje:"buscado..." + id
-    })
+    try {
+        //resibir el id a buscar
+        let id = peticion.params.id;
+    
+        let havitacion = await leerReserva(id)
+    
+        respuesta.status(200).json({
+            estado: true,
+            mensaje:"buscado..." + id
+        })
+    
+    } catch (error){
+        respuesta.status(400).json({
+            estado: false,
+            mensaje:"Error" + error
+        })
+    
+    }
 }
 
-function editarReserva(peticion=request, respuesta=response) {
+async function editarReserva(peticion=request, respuesta=response) {
 
-    let id = peticion.params.id;
+    try {
 
-    respuesta.json({
-        mensaje:"editando..." +id
-    })
+        let id = peticion.params.id
+        let datosPeticion = peticion.body
+
+        await modificarReserva(id, datosPeticion)
+
+        respuesta.status(200).json({
+            estado: true,
+            mensaje: "Exito editando la havitacion"
+        })
+
+
+    } catch (error) {
+        respuesta.status(400).json({
+            estado: false,
+            mensaje: "Upss... tenemos un problema: " + error
+        })
+    }
 }
 function eliminarReserva(peticion=request, respuesta=response) {
     
-    let id = peticion.params.id;
-    
-    respuesta.json({
-        mensaje:"eliminando..."
-    })
+    try {
+
+        let id = peticion.params.id
+
+        await borrarReserva(id)
+
+        respuesta.status(200).json({
+            estado: true,
+            mensaje: "Exito al borrar la havitacion"
+        })
+
+
+    } catch (error) {
+        respuesta.status(400).json({
+            estado: false,
+            mensaje: "Upss... tenemos un problema: " + error
+        })
+    } 
 }
 
 module.exports={
